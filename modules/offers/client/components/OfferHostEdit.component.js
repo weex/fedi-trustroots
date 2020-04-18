Trustroots/trustroots

make the component work, with minimal UX

modules/offers/client/components/OfferHostEdit.component.js
@@ -1,0 +1,121 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import LoadingIndicator from '@/modules/core/client/components/LoadingIndicator';
+import OfferHostEditPresentational from './OfferHostEditPresentational';
+import * as offersApi from '../api/offers.api';
+
+const api = { offers: offersApi };
+
+// as defined in modules/core/client/services/location.client.service.js
+const DEFAULT_LOCATION = {
+  lat: 48.6908333333,
+  lng: 9.14055555556,
+};
+
+export default function OfferHostEdit({ user }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [offerId, setOfferId] = useState();
+  const [status, setStatus] = useState('yes');
+  const [maxGuests, setMaxGuests] = useState(1);
+  const [description, setDescription] = useState('');
+  const [noOfferDescription, setNoOfferDescription] = useState('');
+  const [location, setLocation] = useState(DEFAULT_LOCATION);
+  const [isDefaultLocation, setIsDefaultLocation] = useState(true);
+  const [firstTimeAround, setFirstTimeAround] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+
+      // load host offer from api
+      const [offer] = await api.offers.readByUserId(user._id, 'host');
+
+      // sometimes the offer is not set, yet
+      if (!offer) setFirstTimeAround(true);
+
+      // set the offer values, or defaults
+      setOfferId(offer?._id ?? null);
+      setStatus(offer?.status ?? 'yes');
+      setMaxGuests(offer?.maxGuests ?? 1);
+      setDescription(offer?.description ?? '');
+      setNoOfferDescription(offer?.noOfferDescription ?? '');
+      if (offer?.location) {
+        setLocation(offer.location);
+        setIsDefaultLocation(false);
+      } else {
+        setLocation(DEFAULT_LOCATION);
+        setIsDefaultLocation(true);
+      }
+
+      // overwrite status with the value from query string
+      // @TODO use react-router-dom location in the future
+      const urlParams = new URLSearchParams(window.location.search);
+      const queryStatus = urlParams.get('status');
+
+      if (['yes', 'maybe', 'no'].includes(queryStatus)) setStatus(queryStatus);
+
+      // loading is finished
+      setIsLoading(false);
+    })();
+  }, [user]);
+
+  const handleChangeMaxGuests = (guests, operation) => {
+    switch (operation) {
+      case '+':
+        setMaxGuests(maxGuests => maxGuests + guests);
+        break;
+      case '-':
+        setMaxGuests(maxGuests => maxGuests - guests);
+        break;
+      case '=':
+        setMaxGuests(guests);
+        break;
+      default:
+    }
+  };
+
+  const handleSubmit = async () => {
+    const { lat, lng } = location;
+    const data = {
+      status,
+      maxGuests,
+      description,
+      noOfferDescription,
+      location: [lat, lng],
+    };
+    if (offerId) {
+      await api.offers.update(offerId, data);
+    } else {
+      await api.offers.create({ ...data, type: 'host' });
+    }
+    const nextUrl =
+      window.innerWidth < 768
+        ? `/profile/${user.username}/accommodation`
+        : `/profile/${user.username}`;
+    window.location.assign(nextUrl);
+  };
+
+  if (isLoading) return <LoadingIndicator />;
+
+  return (
+    <OfferHostEditPresentational
+      status={status}
+      maxGuests={maxGuests}
+      description={description}
+      noOfferDescription={noOfferDescription}
+      location={location}
+      firstTimeAround={firstTimeAround}
+      isDefaultLocation={isDefaultLocation}
+      onChangeStatus={setStatus}
+      onChangeMaxGuests={handleChangeMaxGuests}
+      onChangeDescription={setDescription}
+      onChangeNoOfferDescription={setNoOfferDescription}
+      onChangeLocation={setLocation}
+      onSubmit={handleSubmit}
+    />
+  );
+}
+
+OfferHostEdit.propTypes = {
+  user: PropTypes.object.isRequired,
+};

modules/offers/client/components/OfferHostEditPresentational.js
@@ -1,0 +1,96 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function OfferHostEditPresentational({
+  status,
+  maxGuests,
+  description,
+  noOfferDescription,
+  location,
+  firstTimeAround,
+  isDefaultLocation,
+  onChangeStatus,
+  onChangeMaxGuests,
+  onChangeDescription,
+  onChangeNoOfferDescription,
+  onChangeLocation,
+  onSubmit,
+}) {
+  return (
+    <ul>
+      <li>first time around: {JSON.stringify(firstTimeAround)}</li>
+      <li>
+        status:
+        <select
+          value={status}
+          onChange={event => onChangeStatus(event.target.value)}
+        >
+          <option value="yes">yes</option>
+          <option value="maybe">maybe</option>
+          <option value="no">no</option>
+        </select>
+      </li>
+      <li>
+        max guests:
+        <button onClick={() => onChangeMaxGuests(1, '-')}>-</button>
+        <input
+          type="number"
+          min="1"
+          step="1"
+          value={maxGuests}
+          onChange={event => onChangeMaxGuests(+event.target.value, '=')}
+        />
+        <button onClick={() => onChangeMaxGuests(1, '+')}>+</button>
+      </li>
+      <li>
+        description:
+        <textarea
+          value={description}
+          onChange={event => onChangeDescription(event.target.value)}
+        />
+      </li>
+      <li>
+        no offer description:
+        <textarea
+          value={noOfferDescription}
+          onChange={event => onChangeNoOfferDescription(event.target.value)}
+        />
+      </li>
+      <li>is default location: {JSON.stringify(isDefaultLocation)}</li>
+      <li>
+        location:
+        <input
+          value={location.lat}
+          onChange={event =>
+            onChangeLocation({ ...location, lat: +event.target.value })
+          }
+        />
+        <input
+          value={location.lng}
+          onChange={event =>
+            onChangeLocation({ ...location, lng: +event.target.value })
+          }
+        />
+      </li>
+      <li>
+        <button onClick={onSubmit}>Save and Exit</button>
+      </li>
+    </ul>
+  );
+}
+
+OfferHostEditPresentational.propTypes = {
+  status: PropTypes.oneOf(['yes', 'maybe', 'no']).isRequired,
+  maxGuests: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  noOfferDescription: PropTypes.string.isRequired,
+  location: PropTypes.object.isRequired,
+  firstTimeAround: PropTypes.bool.isRequired,
+  isDefaultLocation: PropTypes.bool.isRequired,
+  onChangeStatus: PropTypes.func.isRequired,
+  onChangeMaxGuests: PropTypes.func.isRequired,
+  onChangeDescription: PropTypes.func.isRequired,
+  onChangeNoOfferDescription: PropTypes.func.isRequired,
+  onChangeLocation: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};

weex/fedi-trustroots

modules/offers/client/components/OfferHostEdit.component.js
@@ -1,0 +1,117 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import LoadingIndicator from '@/modules/core/client/components/LoadingIndicator';
+import OfferHostEditPresentational from './OfferHostEditPresentational';
+import * as offersApi from '../api/offers.api';
+
+const api = { offers: offersApi };
+
+// as defined in modules/core/client/services/location.client.service.js
+const DEFAULT_LOCATION = [48.6908333333, 9.14055555556];
+
+export default function OfferHostEdit({ user }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [offerId, setOfferId] = useState();
+  const [status, setStatus] = useState('yes');
+  const [maxGuests, setMaxGuests] = useState(1);
+  const [description, setDescription] = useState('');
+  const [noOfferDescription, setNoOfferDescription] = useState('');
+  const [location, setLocation] = useState(DEFAULT_LOCATION);
+  const [isDefaultLocation, setIsDefaultLocation] = useState(true);
+  const [firstTimeAround, setFirstTimeAround] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+
+      // load host offer from api
+      const [offer] = await api.offers.getOffers(user._id, 'host');
+
+      // sometimes the offer is not set, yet
+      if (!offer) setFirstTimeAround(true);
+
+      // set the offer values, or defaults
+      setOfferId(offer?._id ?? null);
+      setStatus(offer?.status ?? 'yes');
+      setMaxGuests(offer?.maxGuests ?? 1);
+      setDescription(offer?.description ?? '');
+      setNoOfferDescription(offer?.noOfferDescription ?? '');
+      if (offer?.location) {
+        setLocation(offer.location);
+        setIsDefaultLocation(false);
+      } else {
+        setLocation(DEFAULT_LOCATION);
+        setIsDefaultLocation(true);
+      }
+
+      // overwrite status with the value from query string
+      // @TODO use react-router-dom location in the future
+      const urlParams = new URLSearchParams(window.location.search);
+      const queryStatus = urlParams.get('status');
+
+      if (['yes', 'maybe', 'no'].includes(queryStatus)) setStatus(queryStatus);
+
+      // loading is finished
+      setIsLoading(false);
+    })();
+  }, [user]);
+
+  const handleChangeMaxGuests = (guests, operation) => {
+    switch (operation) {
+      case '+':
+        setMaxGuests(maxGuests => maxGuests + guests);
+        break;
+      case '-':
+        setMaxGuests(maxGuests => maxGuests - guests);
+        break;
+      case '=':
+        setMaxGuests(guests);
+        break;
+      default:
+    }
+  };
+
+  const handleSubmit = async () => {
+    const data = {
+      status,
+      maxGuests,
+      description,
+      noOfferDescription,
+      location,
+    };
+    if (offerId) {
+      await api.offers.update(offerId, data);
+    } else {
+      await api.offers.create({ ...data, type: 'host' });
+    }
+    const nextUrl =
+      window.innerWidth < 768
+        ? `/profile/${user.username}/accommodation`
+        : `/profile/${user.username}`;
+    window.location.assign(nextUrl);
+  };
+
+  if (isLoading) return <LoadingIndicator />;
+
+  return (
+    <OfferHostEditPresentational
+      status={status}
+      maxGuests={maxGuests}
+      description={description}
+      noOfferDescription={noOfferDescription}
+      location={location}
+      firstTimeAround={firstTimeAround}
+      isDefaultLocation={isDefaultLocation}
+      onChangeStatus={setStatus}
+      onChangeMaxGuests={handleChangeMaxGuests}
+      onChangeDescription={setDescription}
+      onChangeNoOfferDescription={setNoOfferDescription}
+      onChangeLocation={setLocation}
+      onSubmit={handleSubmit}
+    />
+  );
+}
+
+OfferHostEdit.propTypes = {
+  user: PropTypes.object.isRequired,
+};

modules/offers/client/components/OfferHostEditPresentational.js
@@ -1,0 +1,96 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function OfferHostEditPresentational({
+  status,
+  maxGuests,
+  description,
+  noOfferDescription,
+  location,
+  firstTimeAround,
+  isDefaultLocation,
+  onChangeStatus,
+  onChangeMaxGuests,
+  onChangeDescription,
+  onChangeNoOfferDescription,
+  onChangeLocation,
+  onSubmit,
+}) {
+  return (
+    <ul>
+      <li>first time around: {JSON.stringify(firstTimeAround)}</li>
+      <li>
+        status:
+        <select
+          value={status}
+          onChange={event => onChangeStatus(event.target.value)}
+        >
+          <option value="yes">yes</option>
+          <option value="maybe">maybe</option>
+          <option value="no">no</option>
+        </select>
+      </li>
+      <li>
+        max guests:
+        <button onClick={() => onChangeMaxGuests(1, '-')}>-</button>
+        <input
+          type="number"
+          min="1"
+          step="1"
+          value={maxGuests}
+          onChange={event => onChangeMaxGuests(+event.target.value, '=')}
+        />
+        <button onClick={() => onChangeMaxGuests(1, '+')}>+</button>
+      </li>
+      <li>
+        description:
+        <textarea
+          value={description}
+          onChange={event => onChangeDescription(event.target.value)}
+        />
+      </li>
+      <li>
+        no offer description:
+        <textarea
+          value={noOfferDescription}
+          onChange={event => onChangeNoOfferDescription(event.target.value)}
+        />
+      </li>
+      <li>is default location: {JSON.stringify(isDefaultLocation)}</li>
+      <li>
+        location:
+        <input
+          value={location[0]}
+          onChange={event =>
+            onChangeLocation([+event.target.value, location[1]])
+          }
+        />
+        <input
+          value={location[1]}
+          onChange={event =>
+            onChangeLocation([location[0], +event.target.value])
+          }
+        />
+      </li>
+      <li>
+        <button onClick={onSubmit}>Save and Exit</button>
+      </li>
+    </ul>
+  );
+}
+
+OfferHostEditPresentational.propTypes = {
+  status: PropTypes.oneOf(['yes', 'maybe', 'no']).isRequired,
+  maxGuests: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  noOfferDescription: PropTypes.string.isRequired,
+  location: PropTypes.array.isRequired,
+  firstTimeAround: PropTypes.bool.isRequired,
+  isDefaultLocation: PropTypes.bool.isRequired,
+  onChangeStatus: PropTypes.func.isRequired,
+  onChangeMaxGuests: PropTypes.func.isRequired,
+  onChangeDescription: PropTypes.func.isRequired,
+  onChangeNoOfferDescription: PropTypes.func.isRequired,
+  onChangeLocation: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};